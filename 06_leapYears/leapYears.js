7b4842ab18467a2eb717f84616a34502658f62bb
ninotermulo <ninotermulo01@gmail.com>
2023-07-22 22:37:19 +0800

    Finish leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+// GOAL:
+// Create a function that checks if a giver year is a leap year or not
 
-};
+// PLAN:
+// Criteria for a leap year
+// 1. Year that can be divided by 4
+// 2. Year that can be divided 100 are not leap year unless they are divisible by 400
+
+// Version 1
+// const leapYears = function(year) {
+//     if (year % 4 == 0) {
+//         if (year % 100 == 0 && year % 400 != 0) return false;    
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+// Version 2
+const leapYears = function(year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
