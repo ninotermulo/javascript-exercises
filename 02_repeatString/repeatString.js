1cb421beb3bf0492acfbe6add53a6712860893a8
ninotermulo <ninotermulo01@gmail.com>
2023-07-22 11:49:55 +0800

    Finish answering repeatString JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
+const repeatString = function(string, frequency) {
+    // First Answer
+    // let finalString = '';
+    // switch (true) {
+    //     case frequency == 0:
+    //         return finalString;
+    //     case frequency < 0:
+    //         return 'ERROR';
+    //     default:
+    //         for (let i = 1; i <= frequency; i++) {
+    //             finalString += string;
+    //         }
+    //         return finalString;       
+    // }
 
-};
+    // Modified
+    if (frequency < 0) return 'ERROR';
+    let finalString = '';
+    for (let i = 1; i <= frequency; i++) {
+        finalString += string
+    }
+    return finalString;
+}
+
+console.log(repeatString('Hello', 10));
 
 // Do not edit below this line
 module.exports = repeatString;
+
