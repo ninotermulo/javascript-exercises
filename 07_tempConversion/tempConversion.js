345b3aeef7981f9d84c2fe74970623b5ee74e486
ninotermulo <ninotermulo01@gmail.com>
2023-07-22 23:24:09 +0800

    Finish tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+// GOAL:
+// Create two functions that convert fahrenheit to celcius and vice versa and round output
+// to 1 decimal place
+
+const convertToCelsius = function(fahrenheit) { 
+  return Math.round((fahrenheit - 32) * 5 / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Math.round((celcius * 9 / 5 + 32) * 10) / 10;
 };
+
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
